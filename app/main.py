68e8059e09f9148728a9e593cb0a651f4092c297
Alex Maldonado <alex.maldonado@outlook.com>
2023-01-23 08:57:49 -0300

    Generating a pie chart with world population

--- app/main.py
+++ app/main.py
@@ -5,16 +5,18 @@ import charts
 
 def run():
 
-    data = read_csv.read_csv('./app/world_population.csv')
+    data                   = read_csv.read_csv('./app/world_population.csv')
+    countries, percentages = utils.get_world_percentages(data)
 
-    country = input('Type country => ')
+    charts.generate_pie_chart(countries, percentages)
+    # country = input('Type country => ')
 
-    result = utils.population_by_country(data, country)
+    # result = utils.population_by_country(data, country)
     
-    if len(result) > 0:
-        country = result[0]
-        labels, values = utils.get_population(country)
-        charts.generate_bar_chart(labels, values)
+    # if len(result) > 0:
+    #     country = result[0]
+    #     labels, values = utils.get_population(country)
+    #     charts.generate_bar_chart(labels, values)
 #* Entry point
 if __name__ == '__main__':
     run()
--- app/utils.py
+++ app/utils.py
@@ -18,3 +18,13 @@ def population_by_country(data, country):
     result = filter(lambda item : item['Country/Territory'] == country, data)
     result = list(result)
     return result
+
+
+def get_world_percentages(data):
+    percentages_dict = {item["Country/Territory"]: item["World Population Percentage"] for item in data}
+    
+
+    names = percentages_dict.keys()
+    per = percentages_dict.values()
+
+    return names, per
